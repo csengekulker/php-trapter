159f13ee81f7243fbdb040a6a3f9d1333f026a4c
csengekulker <k71608883429@ktch.hu>
2022-05-12 10:05:46 +0200

    result is displayed on screen

--- src/result.php
+++ src/result.php
@@ -1,10 +1,29 @@
 <?php
 
-$a = $_GET['a'];
-$b = $_GET['b'];
-$c = $_GET['c'];
-$d = $_GET['d'];
-echo $a;
+$a = intval($_GET['a']);
+$b = intval($_GET['b']);
+$c = intval($_GET['c']);
+$d = intval($_GET['d']);
 
+$rootBase = ($a + $b - $c + $d) * 
+($a - $b - $c + $d) * 
+($a + $b - $c - $d) *
+(-$a + $b + $c + $d);
+
+$area = 0;
+
+if ($rootBase < 0) {
+  echo "complex";
+} else {
+  $root = sqrt($rootBase);
+  $area = ($a + $c) * $root / (4 * ($a - $c));
+  // echo $area;
+}
+
+echo file_get_contents("templates/head.html");
+$page =  file_get_contents("templates/result.html");
+$page = str_replace('{{ area }}', $area, $page);
+echo $page;
+echo file_get_contents("templates/foot.html");
 
 ?>
