ed2223164ec413ddb06677b983249bc8ee2d4fb4
csengekulker <k71608883429@ktch.hu>
2022-05-16 09:31:53 +0200

    Created table and test

--- db/init.sql
+++ db/init.sql
@@ -4,3 +4,14 @@ collate utf8_hungarian_ci;
 
 grant all privileges on trapter.*
 to root@localhost identified by '';
+
+use trapter;
+
+create table calc (
+    id int not null primary key auto_increment,
+    a_side int,
+    b_side int,
+    c_side int,
+    d_side int,
+    area double
+);
--- src/includes/data.php
+++ src/includes/data.php
@@ -2,4 +2,15 @@
 include_once("mariadb.php");
 
 $conn = connectDB();
+
+function insertData($conn, $a, $b, $c, $d, $area) {
+    $sql = <<<EOT
+
+    insert into calc (
+        a_side, b_side, c_side, d_side, area
+    ) values ($a, $b, $c, $d, $area)
+
+EOT;
+    $conn->query($sql);
+}
 ?>
